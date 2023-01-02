0c809b586f00ec62c1c88e9cf8f2b91e58620806
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2023-01-02 21:29:58 +0100

    Remove token

--- sonarScan.sh
+++ sonarScan.sh
@@ -5,7 +5,7 @@ unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
 export PATH=$SONAR_SCANNER_HOME/bin:$PATH
 export SONAR_SCANNER_OPTS="-server"
 
-SONAR_TOKEN=sqp_f1d6258593bb0c12081332e91d23760770020b5b sonar-scanner \
+sonar-scanner \
   -Dsonar.projectKey=DB \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://lion:9000
