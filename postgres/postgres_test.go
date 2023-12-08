283c61032b73a44fc49c533f7cb3525fb14aecd1
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2023-12-08 19:20:56 +0100

    Use correct name for test tablte test

--- postgres/postgres_test.go
+++ postgres/postgres_test.go
@@ -102,7 +102,7 @@ func TestPostgresInit(t *testing.T) {
 	sort.Strings(m)
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"albumpictures", "albums", "picturelocations",
-		"pictures", "teststructtabledata", "testtabledata", "testtabledata"}, m)
+		"pictures", "teststructtabledata", "teststructuser", "testtabledata"}, m)
 }
 
 func TestPostgresTableColumns(t *testing.T) {
