cfa8341fba11783d81f3770595d924480291b462
Thorsten A. Knieling <thorsten.knieling@googlemail.com>
2024-11-20 14:01:02 +0100

    Fix test template

--- mysql/mysql_test.go
+++ mysql/mysql_test.go
@@ -97,7 +97,7 @@ func TestMysqlInit(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"AlbumPictures", "Albums", "PictureLocations",
 		"PictureMetadata", "PictureTag",
-		"PictureTags", "Pictures", "TestCreateAdaptTable", "Tags",
+		"PictureTags", "Pictures", "Tags", "TestCreateAdaptTable",
 		"TestStructTableData", "TestTableData"}, m)
 }
 
